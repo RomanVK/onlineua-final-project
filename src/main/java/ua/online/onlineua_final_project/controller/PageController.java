1700130ebc8a0b49ac840b61cda9383df0885968
RomanVK <roman.kuzmin3333@gmail.com>
2021-07-31 22:24:02 +0300

    made login page with localization

--- src/main/java/ua/online/onlineua_final_project/controller/PageController.java
+++ src/main/java/ua/online/onlineua_final_project/controller/PageController.java
@@ -1,13 +1,13 @@
 package ua.online.onlineua_final_project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-    @RequestMapping("/")
-    public String mainPage(){
-        return "index.html";
+    @RequestMapping(value = {"/", "login"})
+    public String staticResource(Model model){
+        return "login";
     }
-
 }
