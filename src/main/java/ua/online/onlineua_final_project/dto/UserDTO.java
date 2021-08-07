44bafe81d1718e5130b80361b64bee5145f7c7be
RomanVK <roman.kuzmin3333@gmail.com>
2021-08-07 11:21:23 +0300

    made UserDTO

---
+++ src/main/java/ua/online/onlineua_final_project/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package ua.online.onlineua_final_project.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class UserDTO {
+    private String email;
+    private String password;
+}
