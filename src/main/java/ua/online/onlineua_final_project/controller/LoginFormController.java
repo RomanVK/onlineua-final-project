455e494fefec09c03d58bafbd9c8cf3c80a4f092
RomanVK <roman.kuzmin3333@gmail.com>
2021-08-07 18:43:38 +0300

    added ExceptionHandler

--- src/main/java/ua/online/onlineua_final_project/controller/LoginFormController.java
+++ src/main/java/ua/online/onlineua_final_project/controller/LoginFormController.java
@@ -3,10 +3,9 @@ package ua.online.onlineua_final_project.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ua.online.onlineua_final_project.dto.UserDTO;
-import ua.online.onlineua_final_project.entity.RoleType;
-import ua.online.onlineua_final_project.entity.User;
 import ua.online.onlineua_final_project.service.LoginFormService;
 
 @Slf4j
@@ -26,11 +25,10 @@ public class LoginFormController {
     public void loginFormController(UserDTO user){
         log.info("{}", user);
         log.info("{}", loginFormService.findUserByEmail(user));
-//        loginFormService.saveNewUser(User.builder()
-//                .firstName("Roman")
-//                .lastName("Kuzmin")
-//                .email("roman.kuzmin3333@gmail.com")
-//                .role(RoleType.USER)
-//                .build());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 }
