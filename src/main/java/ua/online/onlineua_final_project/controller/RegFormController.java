d580c858888b838a44459741138b0b4e986f982a
RomanVK <roman.kuzmin3333@gmail.com>
2021-08-07 18:47:34 +0300

    used saveNewUser

--- src/main/java/ua/online/onlineua_final_project/controller/RegFormController.java
+++ src/main/java/ua/online/onlineua_final_project/controller/RegFormController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ua.online.onlineua_final_project.dto.NoteDTO;
+import ua.online.onlineua_final_project.entity.RoleType;
+import ua.online.onlineua_final_project.entity.User;
 import ua.online.onlineua_final_project.service.RegFormService;
 
 @Slf4j
@@ -24,6 +26,12 @@ public class RegFormController {
     @PostMapping(value = "registration_form")
     public void registrationFormController(NoteDTO note){
         log.info("{}", note);
+        regFormService.saveNewUser(User.builder()
+                .firstName(note.getFirstName())
+                .lastName(note.getLastName())
+                .email(note.getEmail())
+                .role(RoleType.USER)
+                .build());
     }
 
     @ExceptionHandler(RuntimeException.class)
