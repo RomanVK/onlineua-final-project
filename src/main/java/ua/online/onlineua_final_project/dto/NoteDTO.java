e6e5b493236636eee5df9957a08902dced933874
RomanVK <roman.kuzmin3333@gmail.com>
2021-08-07 12:12:04 +0300

    made NoteDTO

---
+++ src/main/java/ua/online/onlineua_final_project/dto/NoteDTO.java
@@ -0,0 +1,17 @@
+package ua.online.onlineua_final_project.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class NoteDTO {
+
+    private String name;
+
+    private String email;
+
+}
