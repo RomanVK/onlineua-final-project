114ee45fe173f9bd625dc407863f946c3cb67426
RomanVK <roman.kuzmin3333@gmail.com>
2021-08-07 17:02:30 +0300

    corrected code

--- src/main/java/ua/online/onlineua_final_project/repository/UserRepository.java
+++ src/main/java/ua/online/onlineua_final_project/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package ua.online.onlineua_final_project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ua.online.onlineua_final_project.entity.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail (String email);
 }
