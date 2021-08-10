8e67106b8e6df310089a8da14975a855ae3d2f94
RomanVK <roman.kuzmin3333@gmail.com>
2021-08-10 23:05:24 +0300

    made access for different roles

--- src/main/java/ua/online/onlineua_final_project/security/SecurityConfig.java
+++ src/main/java/ua/online/onlineua_final_project/security/SecurityConfig.java
@@ -1,35 +1,43 @@
 package ua.online.onlineua_final_project.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+            .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+            .and()
+            .withUser("user").password(passwordEncoder().encode("user123")).roles("USER")
+            .and()
+            .withUser("librarian").password(passwordEncoder().encode("librarian123")).roles("LIBRARIAN");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/welcome", "/registration_form")
-                    .permitAll()
-                .anyRequest()
-                    .authenticated()
-                .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                .and()
-                .logout()
-                    .permitAll();
+            .authorizeRequests()
+            .antMatchers("/", "/welcome", "/login", "/registration").permitAll()
+            .antMatchers("/catalog", "/profile").authenticated()
+            .antMatchers("/orders").hasAnyRole("ADMIN", "LIBRARIAN")
+            .antMatchers("/admin").hasRole("ADMIN")
+            .and()
+            .httpBasic();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
